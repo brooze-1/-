d73ad3e72150ff6593d9972289cbc443d5abb26f
brooze-1 <79853757+brooze-1@users.noreply.github.com>
2021-03-06 17:38:55 +0800

    Update 从文件中爬取名字.py

--- 从文件中爬取名字.py
+++ 从文件中爬取名字.py
@@ -2,7 +2,8 @@ a=input('请输入要查询的文件（记得添加扩展名）：')
 f=open(a,'r',encoding='utf-8')
 n=input('请输入要查询的姓名：')
 num=0
-for name in f:
+r=f.read()
+for name in r:
     if name.count(n)>0:
         num+=1
         print('{}在其中'.format(n))
